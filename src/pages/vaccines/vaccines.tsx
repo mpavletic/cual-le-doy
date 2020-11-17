ab9440ab59aeccda30843877ac5503f5c8aecd5f
Martin Pavletic <tinchopavl@gmail.com>
2020-11-17 12:55:42 -0300

    Add datetime

--- src/pages/vaccines/vaccines.tsx
+++ src/pages/vaccines/vaccines.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import {
+  IonContent,
+  IonDatetime,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
 import './vaccines.css';
 
 const Vaccines: React.FC = () => {
+  const maxDate = new Date().toISOString();
+  const [bornDate, setBornDate] = useState<string>(new Date().toISOString());
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,6 +28,17 @@ const Vaccines: React.FC = () => {
             <IonTitle size="large">Vacunas</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonItem>
+          <IonLabel>Fecha de nacimiento</IonLabel>
+          <IonDatetime
+            cancelText="Cancelar"
+            displayFormat="DD/MM/YYYY"
+            doneText="Listo"
+            max={maxDate}
+            onIonChange={e => setBornDate(e.detail.value!)}
+            value={bornDate}
+          ></IonDatetime>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
