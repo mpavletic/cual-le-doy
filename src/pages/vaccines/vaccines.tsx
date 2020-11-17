b8ca2345096eb216f73cd259385acae7d30447bf
Martin Pavletic <tinchopavl@gmail.com>
2020-11-17 11:46:44 -0300

    Rename tab 3 to vaccines

--- src/pages/vaccines/vaccines.tsx
+++ src/pages/vaccines/vaccines.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './vaccines.css';
 
-const Tab3: React.FC = () => {
+const Vaccines: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Vacunas</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Vacunas</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab3;
+export default Vaccines;
