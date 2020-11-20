3aeb76ce8eef3ac0d0f28e24d57048d663197157
Martin Pavletic <tinchopavl@gmail.com>
2020-11-20 12:19:15 -0300

    Display a confirm on delete all event

--- src/pages/history/history.tsx
+++ src/pages/history/history.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAlert, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useItems } from '../../services/items.service';
 import BreastFeedItem from '../../components/breastfeed-item';
 import { IItem } from '../../services/IItem';
+import { trashOutline } from 'ionicons/icons';
+
 
 const HistoryPage: React.FC = () => {
     let { getAll } = useItems();
     let [items] = React.useState(getAll());
+    let [showRemoveAllAlert, setShowRemoveAllAlert] = React.useState(false);
+
+    let removeAll = () => {
+        setShowRemoveAllAlert(true);
+    };
 
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
                     <IonTitle>Historial</IonTitle>
+                    <IonButtons slot="secondary" onClick={(e) => removeAll()}>
+                        <IonButton>
+                            <IonIcon slot="icon-only" icon={trashOutline} />
+                        </IonButton>
+                    </IonButtons>
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
@@ -26,6 +38,22 @@ const HistoryPage: React.FC = () => {
                         <BreastFeedItem item={item} key={index} />
                     )}
                 </IonList>
+                <IonAlert
+                    isOpen={showRemoveAllAlert}
+                    onDidDismiss={() => setShowRemoveAllAlert(false)}
+                    header="Atención"
+                    message="Todos los registros se van a borrar. Estás seguro?"
+                    buttons={[{
+                        role: 'cancel',
+                        text: 'Cancelar'
+                    }, {
+                        text: 'Aceptar',
+                        handler: () => {
+                            // remove all
+                        }
+                    }]}
+                />
+
             </IonContent>
         </IonPage>
     );
