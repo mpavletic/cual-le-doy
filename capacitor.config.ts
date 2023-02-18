6dcc07b1e438f31788c85b62f9a96bf8c96063d3
Martin Pavletic <martinpavletic@MacBook-Pro-de-Martin.local>
2023-02-18 15:32:29 -0300

    Initial commit from Ionic starter tabs using angular

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mpavletic.cualledoy',
+  appName: 'Cual le doy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
