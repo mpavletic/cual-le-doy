c07bf0c8daeaac82f922a549e00c7ea873c4f35e
Martin Pavletic <mpavletic.dev@gmail.com>
2023-02-20 12:19:31 -0300

    Add initial version of vaccines page

--- src/app/vaccines/vaccines.page.ts
+++ src/app/vaccines/vaccines.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BornDateService } from '../services/born-date.service';
+import { VaccinesService } from '../services/vaccines.service';
+import { IVaccine } from './IVaccine';
 
 @Component({
   selector: 'app-vaccines-tab',
@@ -7,6 +10,21 @@ import { Component } from '@angular/core';
 })
 export class VaccinesPage {
 
-  constructor() {}
+  public maxDateSupported: string;
+  public bornDate: string;
+  public vaccines: IVaccine[];
 
+  constructor(
+    private vaccinesService: VaccinesService,
+    private bornDateService: BornDateService
+  ) {
+    this.maxDateSupported = new Date().toISOString();
+    this.bornDate = this.bornDateService.getDate();
+    this.vaccines = this.vaccinesService.getVaccinesFrom(this.bornDate);
+  }
+
+  public setBornDate(ev: Event) {
+    this.bornDateService.setDate(this.bornDate);
+    this.vaccines = this.vaccinesService.getVaccinesFrom(this.bornDate);
+  }
 }
