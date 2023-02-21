53b386fa7d5f45512ded80be60f6cf26eb76459e
Martin Pavletic <mpavletic.dev@gmail.com>
2023-02-21 11:48:05 -0300

    Add history page

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IItem } from '../services/IItem';
+import { ItemsService } from '../services/items.service';
 
 @Component({
   selector: 'app-history-tab',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HistoryPage {
 
-  constructor() {}
+  constructor(private itemsService: ItemsService) {}
+
+  public removeAll(): void {
+    const confirmed = window.confirm('Todos los registros se van a borrar. Estás seguro?');
+
+    if (confirmed) {
+      this.itemsService.deleteAll();
+    }
+  }
+
+  public get items(): IItem[] {
+    return this.itemsService.getAll();
+  }
+
+  public deleteItem(item: IItem): void {
+    this.itemsService.deleteItemByDate(item.date);
+  }
 
 }
